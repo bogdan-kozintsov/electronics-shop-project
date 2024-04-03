f3e96fcaa3866e181466d8d04f6f9ceae8b6dafd
bogdan.kozintsov <bogdan.kozintsov@gmail.com>
2024-04-03 18:16:20 +1100

    Initial commit

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def position():
+    return Item("Смартфон", 100, 1)
+
+
+def test_item_init(position):
+    assert position.name == "Смартфон"
+    assert position.price == 100
+    assert position.quantity == 1
+
+
+def test_calculate_total_price(position):
+    assert position.calculate_total_price() == 100
+
+
+def test_apply_discount(position):
+    Item.pay_rate = 0.8
+    position.apply_discount(Item.pay_rate)
+    assert position.price == 80
